7ef50dc3760f179e11caab5c01493e682d933405
drew <drew.duncan@gmail.com>
2019-08-25 23:48:05 +0000

    Simple interaction metrics

---
+++ data_metrics.py
@@ -0,0 +1,36 @@
+import csv
+
+interactions = {}
+with open('data/consolidated/interactions.txt', 'r') as f:
+    r = csv.reader(f)
+    r.next()
+    for i, cid, pid, activity in r:
+        interactions[(cid, pid)] = int(activity)
+
+drugs, proteins = {}, {}
+active, total = 0, 0
+for (cid, pid), activity in interactions.items():
+    if cid not in drugs:
+        drugs[cid] = (0, 0)
+    if pid not in proteins:
+        proteins[pid] = (0, 0)
+    drug_active, drug_total = drugs[cid]
+    protein_active, protein_total = proteins[pid]
+    if activity:
+        drug_active += 1
+        protein_active += 1
+        active += 1
+    drug_total += 1
+    protein_total += 1
+    drugs[cid] = (drug_active, drug_total)
+    proteins[pid] = (protein_active, protein_total)
+    total += 1
+print('Active: %d(%d%%)\n Total: %d' % (active, float(active)/total*100, total))
+print('\nDrugs')
+for cid in drugs:
+    active, total = drugs[cid]
+    print('%s: %d/%d (%d%%)' % (cid, active, total, float(active)/total*100))
+print('\nProteins')
+for pid in proteins:
+    active, total = proteins[pid]
+    print('%s: %d/%d (%d%%)' % (pid, active, total, float(active)/total*100))
